53532ed38a0b52e744a6363abc4381bd00c4965d
Giorgi Dzneladze <gio.dz@icloud.com>
2023-06-21 16:30:47 -0400

    Add React router & instale styled-components, react-router-dom, react-hook-form, axios  packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Personal from "./pages/personal";
+import Experience from "./pages/experience";
+import Completed from "./pages/Completed";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/completed" element={<Completed />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
